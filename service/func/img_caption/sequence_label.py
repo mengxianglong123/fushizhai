5aa3998a2986199fd17bbd0e23476a5d592fe274
menglang <1668689565@qq.com>
2023-11-02 17:03:33 +0800

    f:完成序列标注的集成

---
+++ service/func/img_caption/sequence_label.py
@@ -0,0 +1,22 @@
+from modelscope.models import Model
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import Tasks
+# Version less than 1.1 please use TokenClassificationPreprocessor
+from modelscope.preprocessors import TokenClassificationTransformersPreprocessor
+
+
+class SequenceLabel:
+    def __init__(self):
+        model_id = 'damo/nlp_structbert_part-of-speech_chinese-base'
+        model = Model.from_pretrained(model_id)
+        tokenizer = TokenClassificationTransformersPreprocessor(model.model_dir)
+        self.pipeline_ins = pipeline(task=Tasks.token_classification, model=model, preprocessor=tokenizer)
+
+    def get_tags(self, input):
+        """
+        获取词性标注列表
+        :param input:
+        :return:
+        """
+        result = self.pipeline_ins(input=input)
+        return result['output']
