794958e626c7349cabb4587276818982d57bd60c
menglang <1668689565@qq.com>
2023-11-13 15:48:29 +0800

    f:添加新的翻译推理模块

---
+++ service/func/translate/wenyan_translation.py
@@ -0,0 +1,38 @@
+from transformers import (
+    EncoderDecoderModel,
+    AutoTokenizer
+)
+import torch
+from env import CUR_PATH
+
+
+# 基础环境
+PRETRAINED = CUR_PATH + "/static/models/wenyan-translation"
+tokenizer = AutoTokenizer.from_pretrained(PRETRAINED)
+model = EncoderDecoderModel.from_pretrained(PRETRAINED)
+
+
+def inference(text):
+    """
+    文言文翻译
+    :param text:
+    :return:
+    """
+    tk_kwargs = dict(
+        truncation=True,
+        max_length=128,
+        padding="max_length",
+        return_tensors='pt')
+
+    inputs = tokenizer([text, ], **tk_kwargs)
+    model.eval()
+    with torch.no_grad():
+        return tokenizer.batch_decode(
+            model.generate(
+                inputs.input_ids,
+                attention_mask=inputs.attention_mask,
+                num_beams=3,
+                bos_token_id=101,
+                eos_token_id=tokenizer.sep_token_id,
+                pad_token_id=tokenizer.pad_token_id,
+            ), skip_special_tokens=True)[0].replace(" ","")
---
+++ service/main.py
@@ -0,0 +1,6 @@
+import os
+import func.translate.wenyan_translation as trans
+
+
+
+print(trans.inference("此诚危急存亡之秋也。"))
