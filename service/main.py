14f46f92e2b0df734bd9187b36ddf0a5a8aa7a4e
menglang <1668689565@qq.com>
2023-11-16 09:38:24 +0800

    f:完成图片上传

--- service/main.py
+++ service/main.py
@@ -1,10 +1,11 @@
 from flask import Flask, Blueprint, request
-from api import creation
+from api import creation, file
 # 用当前脚本名称实例化Flask对象，方便flask从该脚本文件中获取需要的内容
 app = Flask(__name__)
 
 # 添加蓝图
 app.register_blueprint(creation.creation)  # 创作模块
+app.register_blueprint(file.file)  # 文件服务
 
 app.run(host="0.0.0.0")
 
